e5fb9927629b1114712ae7921e7c17d3521c54ef
anoop <anoopvishnu007@gmail.com>
2020-04-30 14:46:05 +0530

    corrrected int test class name

--- src/intTest/java/com/anoop/transformpdf/TransformPdfApplicationIntTests.java
+++ src/intTest/java/com/anoop/transformpdf/TransformPdfApplicationIntTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class TransformPdfApplicationTests {
+public class TransformPdfApplicationIntTests {
 
 	@Test
 	public void contextLoads() {
